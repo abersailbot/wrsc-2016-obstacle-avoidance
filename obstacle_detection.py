4d5895ebbab25f9f8bbf2ca89206719d336185e0
thip <david@thecappers.co.uk>
2016-09-09 11:39:30 +0100

    doot doot doot im a boot

--- obstacle_detection.py
+++ obstacle_detection.py
@@ -12,11 +12,13 @@ class obstacle_detector:
 	(_,frame) = self.camera.read()
 	(_,frame) = self.camera.read()
 	(_,frame) = self.camera.read()
-
-	mask = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), np.array((0,100,75)), np.array((40,250,255)))
+	
+	frame = cv2.imread("puc.png")
+	mask = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), np.array((150,25,25)), np.array((255,200,200))) \
+        + cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), np.array((0,100,75)), np.array((40,250,255)))
 	kernel = np.ones((5,5),np.uint8)
-	mask = cv2.erode(mask,kernel,iterations=5)
-	mask = cv2.dilate(mask,kernel, iterations=5)
+	mask = cv2.erode(mask,kernel,iterations=1)
+	mask = cv2.dilate(mask,kernel, iterations=1)
 	(cnts, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 	cv2.drawContours(frame, cnts, -1, (0,255,0), 3)
 	cv2.imshow("Keypoints", frame)
