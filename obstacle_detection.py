523bf8f73246184965a87ade41c9b4a2d7d8e640
Louis Taylor <louis@kragniz.eu>
2016-09-09 12:21:03 +0100

    Start merging gopro capture and object detection

--- obstacle_detection.py
+++ obstacle_detection.py
@@ -2,30 +2,31 @@
 
 import cv2
 import numpy as np
+import socket
+import urllib2
 
-class obstacle_detector:
-    def __init__(self):
-	
-	self.camera = cv2.VideoCapture(0)
+GoProIP = "10.5.5.9"
+GoProPort = 8554
+Message = "_GPHD_:0:0:2:0\n"
 
-    def check_for_obstacle(self):
-	(_,frame) = self.camera.read()
-	(_,frame) = self.camera.read()
-	(_,frame) = self.camera.read()
-	
-	frame = cv2.imread("puc.png")
-	mask = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), np.array((150,25,25)), np.array((255,200,200))) \
-        + cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), np.array((0,100,75)), np.array((40,250,255)))
-	kernel = np.ones((5,5),np.uint8)
-	mask = cv2.erode(mask,kernel,iterations=1)
-	mask = cv2.dilate(mask,kernel, iterations=1)
-	(cnts, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	cv2.drawContours(frame, cnts, -1, (0,255,0), 3)
-	cv2.imshow("Keypoints", frame)
-	cv2.waitKey(0)
-	
+urllib2.urlopen("http://{0}/gp/gpControl/execute?p1=gpStream&a1=proto_v2&c1=restart".format(GoProIP)).read()
 
+capture = cv2.VideoCapture("udp://@{0}:{1}".format(GoProIP, GoProPort))
 
-if __name__ == '__main__':
-    detector = obstacle_detector()
-    detector.check_for_obstacle()
+def _process_image(img):
+    mask = cv2.inRange(cv2.cvtColor(img, cv2.COLOR_BGR2HSV), np.array((150,25,25)), np.array((255,200,200))) \
+    + cv2.inRange(cv2.cvtColor(img, cv2.COLOR_BGR2HSV), np.array((0,100,75)), np.array((40,250,255)))
+    kernel = np.ones((5,5),np.uint8)
+    mask = cv2.erode(mask,kernel,iterations=1)
+    mask = cv2.dilate(mask,kernel, iterations=1)
+    (cnts, _) = cv2.findContours(mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(img, cnts, -1, (0,255,0), 3)
+    cv2.imshow("Keypoints", img)
+    cv2.waitKey(1)
+    
+
+def check_for_obstacle():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(Message, (GoProIP, GoProPort))
+    ret, img = capture.read()
+    return check_for_obstacle(img)
